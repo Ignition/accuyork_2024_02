b7de7fd4fce31ca525facb8b30537d4a26a858a0
Gareth Lloyd <gareth.lloyd@memgraph.io>
2024-01-21 17:02:00 +0000

    Add error handling

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -7,19 +7,36 @@ pub struct Interpreter {
     values: HashMap<String, f64>,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum InterpreterError {
+    MissingIdentifier,
+}
+
 impl Interpreter {
     pub fn new(values: HashMap<String, f64>) -> Self {
         Self { values }
     }
 
-    pub fn eval(&self, ast: &Expr) -> f64 {
+    pub fn without_values() -> Self {
+        Self {
+            values: Default::default(),
+        }
+    }
+    pub fn eval(&self, ast: &Expr) -> Result<f64, InterpreterError> {
         match ast {
-            Expr::Binary(lhs, BinOp::Add, rhs) => self.eval(lhs) + self.eval(rhs),
-            Expr::Binary(lhs, BinOp::Subtract, rhs) => self.eval(lhs) - self.eval(rhs),
-            Expr::Binary(lhs, BinOp::Multiply, rhs) => self.eval(lhs) * self.eval(rhs),
-            Expr::Binary(lhs, BinOp::Divide, rhs) => self.eval(lhs) / self.eval(rhs),
-            Expr::Number(val) => *val,
-            Expr::Identifier(name) => *self.values.get(name).unwrap(), // What about errors?
+            Expr::Binary(lhs, BinOp::Add, rhs) => Ok(self.eval(lhs)? + self.eval(rhs)?),
+            Expr::Binary(lhs, BinOp::Subtract, rhs) => Ok(self.eval(lhs)? - self.eval(rhs)?),
+            Expr::Binary(lhs, BinOp::Multiply, rhs) => Ok(self.eval(lhs)? * self.eval(rhs)?),
+            Expr::Binary(lhs, BinOp::Divide, rhs) => Ok(self.eval(lhs)? / self.eval(rhs)?),
+            Expr::Number(val) => Ok(*val),
+            Expr::Identifier(name) => self.fetch_identifiers_value(name),
+        }
+    }
+
+    fn fetch_identifiers_value(&self, name: &String) -> Result<f64, InterpreterError> {
+        match self.values.get(name) {
+            None => Err(InterpreterError::MissingIdentifier),
+            Some(value) => Ok(*value),
         }
     }
 }
@@ -32,7 +49,7 @@ mod tests {
     fn one_plus_one() {
         let x = Expr::num(1.) + Expr::num(1.);
         let interpreter = Interpreter::default();
-        assert_eq!(interpreter.eval(&x), 2.0);
+        assert_eq!(interpreter.eval(&x), Ok(2.0));
     }
 
     #[test]
@@ -42,6 +59,6 @@ mod tests {
         let mut values = HashMap::new();
         values.insert("a".to_string(), 2.);
 
-        assert_eq!(Interpreter::new(values).eval(&x), 42.0);
+        assert_eq!(Interpreter::new(values).eval(&x), Ok(42.0));
     }
 }
