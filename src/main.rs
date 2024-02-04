edf5b63c75f56c4ce86857e72d20f72300bd77a9
Gareth Lloyd <gareth.lloyd@memgraph.io>
2024-02-04 20:20:40 +0000

    Borrow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-use accuyork_2024_02::ast::Expr;
-use accuyork_2024_02::interpreter::Interpreter;
+#[derive(Clone)]
+struct S {}
+
+fn by_mut(_: &mut S) {}
+
+fn by_ref(_: &S) {}
+
+fn by_val(_: S) {}
 
 fn main() {
-    let x = Expr::num(1.) + Expr::num(1.);
-    println!("{:?}", Interpreter::default().eval(&x));
+    let mut s = S {};
+    by_mut(&mut s);
+    by_ref(&s);
+    by_val(s);
+
+    // You can have as many immutable references, but only one mutable reference
+
+    // let s1_ref = &s;
+    // let s2_ref = &s;
+    // let s3_ref = &s;
+    // by_mut(&mut s);
+    // by_ref(&s1_ref);
+    // by_ref(&s3_ref);
+    // by_ref(&s2_ref);
 }
